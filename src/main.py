f0059f2757d7d1e2189264c9929916facdf364bc
TheJurmik <thejurmik0@duck.com>
2025-04-18 15:50:36 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 def main():
     say("Hello, World")
     mezera()
+    plus(1, 5)
+    mezera()
 
 def say(text):
     print(text)
+
+def plus(x, y):
+    print(x + y)
 
 def mezera():
     print("--------------")
